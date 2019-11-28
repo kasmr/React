f5d7be4ee7dc0c6d859ab4063421e0c5f09aa9c4
kasmr <alikkras@gmail.com>
2019-11-28 23:56:29 +0300

    initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header branding='Contact Manager' />
+      <div className='container'>
+        <Contacts />
+      </div>
+    </div>
+  );
+}
+
+export default App;
