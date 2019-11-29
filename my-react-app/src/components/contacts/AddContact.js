fb75fc1c7d36453f094ec563da97788b95855d74
kasmr <alikkras@gmail.com>
2019-11-29 16:54:13 +0300

    update

---
+++ my-react-app/src/components/contacts/AddContact.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import uuid from 'uuid';
+import TextInputGroup from '../layout/TextInputGroup';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  onSubmit = (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+
+    const newContact = {
+      id: uuid(),
+      name,
+      email,
+      phone
+    };
+
+    dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+    this.setState({
+      name: '',
+      email: '',
+      phone: ''
+    });
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, phone } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className='card mb-3'>
+              <div className='card-header'>AddContact</div>
+              <div className='card-body'>
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label='Name'
+                    name='name'
+                    placeholder='Enter Name'
+                    value={name}
+                    onChange={this.onChange}
+                  />
+                  <TextInputGroup
+                    label='Email'
+                    name='email'
+                    placeholder='Enter Email'
+                    value={email}
+                    onChange={this.onChange}
+                  />
+                  <TextInputGroup
+                    label='Phone'
+                    name='phone'
+                    placeholder='Enter Phone'
+                    value={phone}
+                    onChange={this.onChange}
+                  />
+
+                  <input
+                    type='submit'
+                    value='Add Contact'
+                    className='btn btn-dark btn-block'
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default AddContact;
