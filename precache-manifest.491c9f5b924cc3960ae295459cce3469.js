df926a1d18a22b88d20d33d873f32cbb23031e66
Alik <alikkras@gmail.com>
2019-12-01 20:52:16 +0300

    Updates

---
+++ precache-manifest.491c9f5b924cc3960ae295459cce3469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fdb93d04004b1b7e6360f91ab41282",
+    "url": "/react/my-react-app/index.html"
+  },
+  {
+    "revision": "6313190d99a59f4ed036",
+    "url": "/react/my-react-app/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "698c52f9d960b0431f8e",
+    "url": "/react/my-react-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6313190d99a59f4ed036",
+    "url": "/react/my-react-app/static/js/2.cbffca23.chunk.js"
+  },
+  {
+    "revision": "698c52f9d960b0431f8e",
+    "url": "/react/my-react-app/static/js/main.2bd28870.chunk.js"
+  },
+  {
+    "revision": "d89054c99409c753437c",
+    "url": "/react/my-react-app/static/js/runtime-main.5ca5b292.js"
+  }
+]);
