254eed65270aa1ac5d789388513e1504af12d85d
Alik <alikkras@gmail.com>
2019-12-01 20:45:01 +0300

    update #2

--- my-react-app/src/components/contacts/EditContact.js
+++ my-react-app/src/components/contacts/EditContact.js
@@ -3,14 +3,28 @@ import { Consumer } from "../../context";
 import uuid from "uuid";
 import TextInputGroup from "../layout/TextInputGroup";
 import axios from "axios";
+import { thisExpression } from "@babel/types";
 
-class AddContact extends Component {
+class EditContact extends Component {
   state = {
     name: "",
     email: "",
     phone: "",
     errors: {}
   };
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    const contact = res.data;
+    this.setState({
+      name: contact.name,
+      email: contact.email,
+      phone: contact.phone
+    });
+  }
 
   onSubmit = async (dispatch, e) => {
     e.preventDefault();
@@ -31,17 +45,19 @@ class AddContact extends Component {
       return;
     }
 
-    const newContact = {
+    const updContact = {
       name,
       email,
       phone
     };
 
-    const res = await axios.post(
-      "https://jsonplaceholder.typicode.com/users",
-      newContact
+    const { id } = this.props.match.params;
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updContact
     );
-    dispatch({ type: "ADD_CONTACT", payload: res.data });
+
+    dispatch({ type: "UPDATE_CONTACT", payload: res.data });
 
     //Clear state
     this.setState({
@@ -65,7 +81,7 @@ class AddContact extends Component {
           const { dispatch } = value;
           return (
             <div className="card mb-3">
-              <div className="card-header">Add Contact</div>
+              <div className="card-header">Edit Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
                   <TextInputGroup
@@ -95,7 +111,7 @@ class AddContact extends Component {
 
                   <input
                     type="submit"
-                    value="Add Contact"
+                    value="Update Contact"
                     className="btn btn-info btn-block"
                   />
                 </form>
@@ -108,4 +124,4 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+export default EditContact;
--- my-react-app/src/context.js
+++ my-react-app/src/context.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'DELETE_CONTACT':
+    case "DELETE_CONTACT":
       return {
         ...state,
         contacts: state.contacts.filter(
           contact => contact.id !== action.payload
         )
       };
-    case 'ADD_CONTACT':
+    case "ADD_CONTACT":
       return {
         ...state,
         contacts: [action.payload, ...state.contacts]
+      };
+    case "UPDATE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id
+            ? (contact = action.payload)
+            : contact
+        )
       };
     default:
       return state;
@@ -23,28 +33,15 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '555-444-4444'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '555-222-2222'
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  async componentDidMount() {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+    this.setState({ contacts: res.data });
+  }
 
   render() {
     return (
