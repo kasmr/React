7a6b580179059ddadb5b65d8ce5a831fdfc4bd23
kasmr <alikkras@gmail.com>
2019-12-21 21:52:46 +0300

    Completed

--- github-search/src/context/alert/AlertState.js
+++ github-search/src/context/alert/AlertState.js
@@ -1,31 +1,32 @@
-// import React, { useReducer } from 'react';
-// import axios from 'axios';
-// import GithubContext from './githubContext';
-// import GithubReducer from './githubReducer';
-// import { SET_ALERT, REMOVE_ALERT } from '../types';
-// import githubContext from './githubContext';
+import React, { useReducer } from 'react';
+import AlertContext from './alertContext';
+import AlertReducer from './alertReducer';
+import { SET_ALERT, REMOVE_ALERT } from '../types';
 
-// const GithubState = props => {
-//   const initialState = null;
+const AlertState = props => {
+  const initialState = null;
 
-//   const [state, dispatch] = useReducer(AlertReducer, initialState);
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
 
-//   //Set alert
-//   const showAlert = (msg, type) => {
-//     setAlert({ msg, type });
-//     setTimeout(() => setAlert(null), 5000);
-//   };
+  //Set alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type }
+    });
+    setTimeout(() => dispatch({ type: REMOVE_ALERT }), 5000);
+  };
 
-//   return (
-//     <AlertContext.Provider
-//       value={{
-//         alert: state,
-//         sertAlert
-//       }}
-//     >
-//       {props.children}
-//     </AlertContext.Provider>
-//   );
-// };
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
 
-// export default AlertState;
+export default AlertState;
